5e0ae56f42f26cca64c291d345288caa19c52641
Kerry Roberts <kerry.h.roberts@gmail.com>
2015-03-23 09:34:10 -0600

    All triangle tests pass

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,16 @@
 class Triangle < Polygon
- 
+
   def initialize(side_length)
     super(3, side_length)
   end
+
+  def perimeter
+    @side_length * 3
+  end
+
+  def area
+      (Math.sqrt(3) / 4) * @side_length**2
+  end
+
 
 end
